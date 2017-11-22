5bac20aacba2488dc8864e82eba7f3858adc052e
Jonathan Smith <jonathan@peoplesized.com>
2017-11-22 13:16:59 +0200

    improved, more checks

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const md5 = require('md5')
 const MongoClient = require('mongodb').MongoClient
 const Logger = require('./Logger').Logger
 const logger = new Logger()
-
+let DRY_RUN = true
 const db_connection = 'mongodb://localhost:27017/douma_production'
 
 const run = async () => {
   let records_initial_count = 0 // done
   let records_groups_with_duplicates = 0
   let records_all_duplicates = 0
-  let records_plan_to_delete = 0
+  let records_plan_to_move = 0
   let records_plan_to_remain = 0
-  let records_deleted = 0
-  let records_expected_remain_after_delete= 0
+  let records_moved = 0
+  let records_expected_remain_after_move= 0
   let records_diff_expected_actual_end = 0
   let records_actual_end_count = 0
+  let records_duplicate_ids = 0
   
   const db = await MongoClient.connect(db_connection)
   const records = db.collection('records')
+  const duplicate_id_records = db.collection('duplicate_id_records')
   
   records_initial_count = await records.count()
 
@@ -47,15 +49,15 @@ const run = async () => {
     records_groups_with_duplicates = number_of_groups_with_more_than_1_record
     records_all_duplicates = count_of_duplicate_records
 
-    const records_plan_to_delete = count_of_duplicate_records - number_of_groups_with_more_than_1_record
+    const records_plan_to_move = count_of_duplicate_records - number_of_groups_with_more_than_1_record
 
     logger.write({
       number_of_groups: number_of_groups_with_more_than_1_record,
       duplicate_records_count: count_of_duplicate_records,
-      records_plan_to_delete: records_plan_to_delete
+      records_plan_to_move: records_plan_to_move
     })
 
-    return records_plan_to_delete
+    return records_plan_to_move
   }
 
   // Take a group {id, count}
@@ -76,23 +78,35 @@ const run = async () => {
     return result
   }
 
-  const delete_everything_but_latest_in_group = async (group) => {
+  const move_everything_but_latest_in_group = async (group) => {
     let output = []
     const group_records = await records.find({id: group._id}).sort({_id: -1}).toArray()
 
     const records_keep = group_records.slice(0,1)
-    const records_remove = group_records.slice(1)
+    const records_move = group_records.slice(1)
 
     if (records_keep.length !== 1) {
       throw new Error('somehow - who knows - nicolai was wrong. there are no records to keep')
-    } else if (records_remove.length < 1) {
-      throw new Error('somehow - who knows - nicolai was wrong. there are no records to remove')
+    } else if (records_move.length < 1) {
+      throw new Error('somehow - who knows - nicolai was wrong. there are no records to move')
     } else {
-      // remove everything in remove
-      const ids_to_delete = records_remove.map(r => r._id)
-      const ids_strings = ids_to_delete.map(id => id.toString())
-      logger.write({delete_ids: ids_strings, delete_ids_count: ids_strings.length, duplicate_record_id: group._id})
-      // await records.deleteMany({_id: {$in: ids_to_delete}})
+      // move everything in move
+      const ids_to_move = records_move.map(r => r._id)
+      const ids_strings = ids_to_move.map(id => id.toString())
+      logger.write({move_ids: ids_strings, move_ids_count: ids_strings.length, duplicate_record_id: group._id})
+
+
+      try {
+        await duplicate_id_records.insertMany(records_move)
+        const result = await records.deleteMany({_id: {$in: ids_to_move}})
+        if (result.deletedCount !== records_move.length) {
+          throw new Error('Stop, have deleted wrong number of records')
+        }
+      } catch (e) {
+        console.log(e || 'Something went wrong inserting into collection duplicate_id_records')
+        process.exit()
+      }
+
       return ids_strings.length
     }
   }
@@ -101,7 +115,7 @@ const run = async () => {
     // Pipeline
   // 1. Find duplicates
   const duplicate_groups = await find_duplicates()
-  records_plan_to_delete = count_duplicate_records(duplicate_groups)
+  records_plan_to_move = count_duplicate_records(duplicate_groups)
 
   // 2. Count exact duplicates in group
   // const some_result = duplicate_groups//.slice(0, 10)
@@ -111,30 +125,37 @@ const run = async () => {
   // const result = await Promise.all(another_result)
   // logger.write({result})
 
-  // 3. Delete everything but latest in each group
-  // const promises = duplicate_groups.slice(0,200).map(async (group) => {
-  // // const promises = duplicate_groups.map(async (group) => {
-  //   return await delete_everything_but_latest_in_group(group)
-  // })
-  // const result = await Promise.all(promises)
-  // records_deleted = result.reduce((acc, i) => acc + i,0 )
+  // 3. Move everything but latest in each group
+  if (!DRY_RUN) {
+    const promises = duplicate_groups.slice(0,1000).map(async (group) => {
+    // const promises = duplicate_groups.map(async (group) => {
+      return await move_everything_but_latest_in_group(group)
+    })
+    const result = await Promise.all(promises)
+    records_moved = result.reduce((acc, i) => acc + i,0 )
+  }
 
+  console.log("\nPRE-FIX STATS\n")
   console.log('records_initial_count',records_initial_count)
   console.log('records_groups_with_duplicates', records_groups_with_duplicates)
   console.log('records_all_duplicates', records_all_duplicates)
-  console.log('records_plan_to_delete',records_plan_to_delete)
-  records_plan_to_remain = records_initial_count - records_plan_to_delete
+  console.log('records_plan_to_move',records_plan_to_move)
+  records_plan_to_remain = records_initial_count - records_plan_to_move
   console.log('records_plan_to_remain', records_plan_to_remain)
-  console.log('records_deleted',records_deleted)
-  records_expected_remain_after_delete= records_initial_count - records_deleted
-  console.log('records_expected_remain_after_delete',records_expected_remain_after_delete)
+  console.log('records_moved',records_moved)
+
+  console.log("\nPOST-FIX STATS\n")
+  records_expected_remain_after_move= records_initial_count - records_moved
+  console.log('records_expected_remain_after_move',records_expected_remain_after_move)
   records_actual_end_count = await records.count()
   console.log('records_actual_end_count',records_actual_end_count)
-  records_diff_expected_actual_end = records_actual_end_count - records_expected_remain_after_delete
+  records_diff_expected_actual_end = records_actual_end_count - records_expected_remain_after_move
   console.log('records_diff_expected_actual_end', records_diff_expected_actual_end)
-  const deleted_correct_number = records_expected_remain_after_delete=== records_actual_end_count
-  console.log('deleted_correct_number', deleted_correct_number)
-  
+  const moved_correct_number = records_expected_remain_after_move=== records_actual_end_count
+  console.log('moved_correct_number', moved_correct_number)
+  records_duplicate_ids = await duplicate_id_records.count()
+  console.log('records_duplicate_ids', records_duplicate_ids)  
 }
 
+DRY_RUN = false
 run()
